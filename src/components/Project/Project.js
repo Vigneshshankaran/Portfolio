ee37d04333e2b560939a77d93eb040916b132bcf
Vigneshshankaran <vigneshganesha@gmail.com>
2022-04-30 12:12:38 +0530

    money

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -100,9 +100,7 @@ and forgot password with email authentication.</p>
                   <h3>Expense Tracker</h3>
                   <p>The MERN Expense Tracker application will allow
                      users to keep track of their day-to-day expenses.
-                     Users who are signed in to their accounts will be
-                     able to add their expense records with details such
-                     as expense description, category, and amount.</p>
+                     </p>
                   <BootstrapTooltip title="frontend source code">
                      <button>  <a href="https://github.com/Vigneshshankaran/Expense-Tracker" target="_blank" rel="noreferrer"><GrGithub className="project-icons" />
                      </a>
